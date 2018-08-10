5e4700440837c096a5c867682d0aa628b41c13a4
Joseph Mulder <joseph.mulder@stonybrook.edu>
2018-08-10 16:43:32 -0400

    changed db info

--- database/config.js
+++ database/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     host: 'localhost',
     user: 'root',
-    password: 'password',
-    database: 'movies',
+    password: 'mymovielistdb',
+    database: 'MyMovieListdb',
     API_KEY: '0d284ed9ae9bea6479b0de0c813b9ec9'
 };
